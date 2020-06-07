395ed3ee207ce69fad868671ea5fec899e954e1f
indy <indy@fixmost.com>
2020-06-07 22:46:13 +0300

    creating github pages project

---
+++ precache-manifest.359d686c00505e1965a6ae0cae580186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883b1ee4acc41ef35a2006271e7b885c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1241b786528339e9663d",
+    "url": "/static/css/2.11a187e6.chunk.css"
+  },
+  {
+    "revision": "13a746867177d4d4f792",
+    "url": "/static/css/main.21d2ac61.chunk.css"
+  },
+  {
+    "revision": "1241b786528339e9663d",
+    "url": "/static/js/2.16511af4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.16511af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a746867177d4d4f792",
+    "url": "/static/js/main.a217be2b.chunk.js"
+  },
+  {
+    "revision": "89200a2629aa4f8c5717",
+    "url": "/static/js/runtime-main.7fba4d06.js"
+  }
+]);
